d0dce4737dd38761671f8316943216609eab72f9
patelb268 <patel.prayag.b@gmail.com>
2024-02-11 11:46:05 -0700

    created db

--- kaizntree_back_end/backend/admin.py
+++ kaizntree_back_end/backend/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Item, Tag
+from .models import Category
+from .models import User
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(User)
--- kaizntree_back_end/backend/serializers.py
+++ kaizntree_back_end/backend/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 from .models import Item
 
 class ItemSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    tags = serializers.StringRelatedField(many=True)
     class Meta:
         model = Item
-        fields = '__all__'
+        fields = ['user','sku', 'name', 'stock_status', 'available_stock', 'category', 'tags']
