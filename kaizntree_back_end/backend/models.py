5ac539e21befd1906b303796bb804a4036d598be
PRAYAG PATEL <patel.prayag.b@gmail.com>
2024-02-11 20:15:20 -0800

    updated models

--- kaizntree_back_end/backend/models.py
+++ kaizntree_back_end/backend/models.py
@@ -1,51 +1,47 @@
 from django.db import models
 
-# Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return self.name
-    
-    class Meta:
-        indexes = [
-            models.Index(fields=['name'])
-        ]
-
-class Tag(models.Model):
-    name = models.CharField(max_length=255, unique=True, db_index=True)
-
-    def __str__(self):
-        return self.name
-    
-    class Meta:
-        indexes = [
-            models.Index(fields=['name'])
-        ]
-
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    user_name = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.user_name
 
 class Item(models.Model):
-    STOCK_STATUS_CHOICES = [
-        ('low', 'Low'),
-        ('empty', 'Empty'),
-    ]
-    
-    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True, related_name='items')
-    sku = models.CharField(max_length=50, primary_key=True)
-    name = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, related_name='items')
-    stock_status = models.CharField(max_length=20, choices=STOCK_STATUS_CHOICES)
-    available_stock = models.IntegerField()
+    id = models.AutoField(primary_key=True)
+    sku = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    in_stock = models.IntegerField(null=False, blank=False)
+    available_stock = models.IntegerField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-   
+
+
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    icon = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TagItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    sku = models.ForeignKey('Item', on_delete=models.CASCADE)
+    tag = models.ForeignKey('Tag', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.tag.name
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
